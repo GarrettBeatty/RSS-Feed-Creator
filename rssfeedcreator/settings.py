4ae183b3426f3e8622f821dcf68fdc87494fa16e
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2018-05-05 22:41:25 -0400

    creator migrations

--- rssfeedcreator/settings.py
+++ rssfeedcreator/settings.py
@@ -6,7 +6,7 @@ import os
 class Config(object):
     """Base configuration."""
 
-    SECRET_KEY = os.environ.get('RSSFEEDCREATOR_SECRET')
+    SECRET_KEY = os.environ.get('rssfeedcreator_secret')
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
