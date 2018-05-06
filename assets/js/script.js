1a26ed3fe271d26e93bedc01cb8d05dec49634c6
Garrett Beatty <gbt@Garretts-MacBook-Pro.local>
2018-05-06 12:03:43 -0400

    fixed linting issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,17 @@
 // App initialization code goes here
-$(function() {
-
-    $('#login-form-link').click(function(e) {
-		$("#loginForm").delay(100).fadeIn(100);
- 		$("#registerForm").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#registerForm").delay(100).fadeIn(100);
- 		$("#loginForm").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
+$(() => {
+  $('#login-form-link').click(function loginclick(e) {
+    $('#loginForm').delay(100).fadeIn(100);
+    $('#registerForm').fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  $('#register-form-link').click(function registerclick(e) {
+    $('#registerForm').delay(100).fadeIn(100);
+    $('#loginForm').fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
 });
